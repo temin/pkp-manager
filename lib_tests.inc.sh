d33fa4f32181fc33a9f7753417d6e8a60c603720
Mitja Podreka <mitja@kitaj.net>
2021-07-26 11:21:59 +0200

    Adding test file

---
+++ lib_tests.inc.sh
@@ -0,0 +1,46 @@
+#####
+# Tests and checks #
+               #####
+case "$1" in
+
+    config_on-load)
+    
+        # Check if application type is specified
+        if [[ -z $pkpApp  && $subcommand != 'sync-backup' ]]; then
+            printf "\n\t%s\n\n" "PKP application is not defined"
+            exit 1
+        fi
+        
+        # To-Do: 
+        # Check/Compare the config-local.inc.sh  and config-local.TEMPLATE.inc.sh files
+        # Variables in both files should be same
+        
+    ;;
+    
+    config_check-dir-file)
+    
+        # Check if necessary directories/files exist
+        checkDirectories=($pkpRootPath $pkpBackupRootPath $pkpWebRootPath $pkpAppBackupRootPath $pkpAppCodePath $pkpAppDataPath $pkpAppStorage $pkpAppDownloads)
+        checkFiles=($backupServerSSHKey $pkpAppDatabaseBackupFile)
+
+        #####
+        # Directories and files checks #
+                                #####
+
+        for d in ${checkDirectories[@]}; do
+            if [[ ! -d $d ]]; then
+                echo -e "\n Folder \e[3m\e[1m$d\e[0m is missing! \n"
+                exit 1
+            fi
+        done
+
+        for f in ${checkFiles[@]}; do
+            if [[ ! -f $f ]]; then
+                echo -e "\n File \e[3m\e[1m$f\e[0m is missing! \n"
+                exit 1
+            fi
+        done
+        
+    ;;
+
+esac
